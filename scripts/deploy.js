f50a85a26a5468d9f97732667ae7193ad9e7d96b
Rodolfo Ferro <rodolfoferroperez@gmail.com>
2023-10-09 15:02:44 -0600

    Updates deployment script.

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -2,7 +2,6 @@ async function main() {
     const [deployer] = await ethers.getSigners();
 
     console.log("Deploying contracts with the account:", deployer.address);
-
     console.log("Account balance:", (await deployer.getBalance()).toString());
 
     const Item = await ethers.getContractFactory("ManaVendingMachine");
